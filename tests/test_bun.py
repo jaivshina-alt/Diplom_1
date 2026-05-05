7bd342d3ac8a02a84269f4d35440497484670593
Julia Ivshina <j.a.ivshina@gmail.com>
2026-05-05 21:54:54 +0400

    edited test_bun.py

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -6,22 +6,22 @@ from praktikum.bun import Bun
 class TestBun:
     #Тесты для класса Bun
 
-    @pytest.mark.parametrize('name, price', [
-        ('black bun', 100),
-        ('white bun', 200.5),
-        ('red bun', 0),
-        ('', 50),
+    @pytest.mark.parametrize('name', [
+        'black bun',
+        'white bun',
+        'red bun',
+        '',
     ])
-    def test_get_name_returns_name_from_constructor(self, name, price):
-        bun = Bun(name, price)
+    def test_get_name_returns_name_from_constructor(self, name):
+        bun = Bun(name, price=100)
         assert bun.get_name() == name
 
-    @pytest.mark.parametrize('name, price', [
-        ('black bun', 100),
-        ('white bun', 200.5),
-        ('red bun', 0),
-        ('cheap bun', 0.01),
+    @pytest.mark.parametrize('price', [
+        100,
+        200.5,
+        0,
+        0.01,
     ])
-    def test_get_price_returns_price_from_constructor(self, name, price):
-        bun = Bun(name, price)
+    def test_get_price_returns_price_from_constructor(self, price):
+        bun = Bun('black bun', price)
         assert bun.get_price() == price
